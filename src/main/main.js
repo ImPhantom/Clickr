9c29e7d8d6a5eed9e15e4ba62ac2d53879c7e69b
ImPhantom <danieljcannon@live.com>
2021-12-27 23:47:37 -0500

    Reformatted project to new eslint config standards

--- src/main/main.js
+++ src/main/main.js
@@ -1,23 +1,23 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
+	app.quit();
 }
 
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
+	// Create the browser window.
+	const mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+	});
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+	// and load the index.html of the app.
+	// eslint-disable-next-line no-undef
+	mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+	// Open the DevTools.
+	mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
@@ -29,17 +29,17 @@ app.on('ready', createWindow);
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
 });
 
 // In this file you can include the rest of your app's specific main process
--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,17 +1,17 @@
 const rules = require('./webpack.rules');
 
 rules.push({
-  test: /\.s[ac]ss$/i,
-  use: [
-    { loader: 'style-loader' }, // creates style nodes
-    { loader: 'css-loader' }, // translates css into commonjs
-    { loader: 'sass-loader' }, // compiles sass/scss
-  ],
+	test: /\.s[ac]ss$/i,
+	use: [
+		{ loader: 'style-loader' }, // creates style nodes
+		{ loader: 'css-loader' }, // translates css into commonjs
+		{ loader: 'sass-loader' }, // compiles sass/scss
+	],
 });
 
 module.exports = {
-  // Put your normal webpack config below here
-  module: {
-    rules,
-  },
+	// Put your normal webpack config below here
+	module: {
+		rules,
+	},
 };
