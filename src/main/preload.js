d71031b89f6f5294e83bb5c435ecab71edb38bb7
ImPhantom <danieljcannon@live.com>
2022-01-03 22:41:18 -0500

    Added channel validation & distinct invoking to our preload methods

--- src/main/preload.js
+++ src/main/preload.js
@@ -1,6 +1,25 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
+const validChannels = {
+	invoke: ['get-stored-value'],
+	send: ['close-window', 'minimize-window', 'update-shortcut', 'arm-toggle'],
+	handle: ['arm-result']
+};
+
 contextBridge.exposeInMainWorld('api', {
-	send: (channel, data) => ipcRenderer.invoke(channel, data),
-	handle: (channel, callable, event, data) => ipcRenderer.on(channel, callable(event, data))
+	invoke: (channel, data) => {
+		if (validChannels.invoke.includes(channel)) {
+			return ipcRenderer.invoke(channel, data);
+		}
+	},
+	send: (channel, data) => {
+		if (validChannels.send.includes(channel)) {
+			ipcRenderer.send(channel, data);
+		}
+	},
+	on: (channel, callable) => {
+		if (validChannels.handle.includes(channel)) {
+			ipcRenderer.on(channel, (_, ...args) => callable(...args));
+		}
+	}
 });
